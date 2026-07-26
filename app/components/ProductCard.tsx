abbd2ea24d58cb0b1c5b5fbced1057d0ce0c7b40
frspach282-creator <faris.ex282@gmail.com>
2026-07-27 02:16:21 +0700

    Test native img

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,11 +4,21 @@ type ProductCardProps = {
   product: Product;
 };
 
-export default function ProductCard({ product }: ProductCardProps) {
+export default function ProductCard({
+  product,
+}: ProductCardProps) {
   return (
-    <div className="border border-white p-6 text-white">
-      <h1>{product.title}</h1>
-      <p>{product.price}</p>
+    <div className="border border-red-500 p-6 text-white">
+      <img
+        src={product.imageFront}
+        alt={product.title}
+        width={300}
+        height={300}
+      />
+
+      <h2>{product.title}</h2>
+
+      <p>Rp {product.price}</p>
     </div>
   );
 }
