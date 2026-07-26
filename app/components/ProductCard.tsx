87e879faec5c7162685d47a185db01bd7ea71ffc
frspach282-creator <faris.ex282@gmail.com>
2026-07-27 02:04:43 +0700

    Minimal ProductCard

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,25 +1,14 @@
-import Image from "next/image";
-import Link from "next/link";
 import type { Product } from "../types/product";
 
 type ProductCardProps = {
   product: Product;
 };
 
-export default function ProductCard({
-  product,
-}: ProductCardProps) {
+export default function ProductCard({ product }: ProductCardProps) {
   return (
-    return (
-  <div className="rounded-xl border border-white p-6 text-white">
-    <p>{product.title}</p>
-
-    <Image
-      src={product.imageFront}
-      alt={product.title}
-      width={200}
-      height={200}
-    />
-  </div>
-);
+    <div className="border border-white p-6 text-white">
+      <h1>{product.title}</h1>
+      <p>{product.price}</p>
+    </div>
+  );
 }
